054f6ccc61647b07c9aaa1a2f5f57493d6d46bdf
averykirkpatrick <kirkae20@wfu.edu>
2022-05-17 12:10:06 -0400

    Added comments, finished implementing expected time portion of my code

--- src/Lab4.java
+++ src/Lab4.java
@@ -16,28 +16,18 @@ public class Lab4 {
 
     // sort method which takes in ArrayList of countries, size of list, and Comparator interface
     public static void sort(ArrayList<Country> list, int n, Comparator<Country> c) {
-        boolean sorted = false;
+        boolean unsorted = true;
         // while list is not sorted
-        while (!sorted) {
+        while (unsorted) {
+            unsorted = false;
             int countSwaps = 0;
             // check adjacent list objects and determine if they should be swapped
             for(int i = 0; i < n - 1; i++) {
-                // if compare returns a negative number, no swap necessary
-                if (c.compare(list.get(i), list.get(i + 1)) <= 0){
-                    // if list has had no swaps yet, it is sorted so far
-                    if (countSwaps == 0) {
-                        sorted = true;
-                    }
-                    // if list receives one swap before finishing iteration, it is not sorted
-                    else {
-                        sorted = false;
-                    }
-                }
-                // if compare returns a positive number, call swap, count that swap occurred, set sorted to false
-                else {
+                // if compare returns a positive number, call swap
+                if (c.compare(list.get(i), list.get(i + 1)) > 0){
                     swap(list, i, i + 1);
                     countSwaps++;
-                    sorted = false;
+                    unsorted = true;
                 }
             }
         }
@@ -61,19 +51,6 @@ public class Lab4 {
         inputFile.nextLine();
         // creates ArrayList to store countries
         ArrayList<Country> CountryList = new ArrayList<Country>();
-        ArrayList<Country> TestList = new ArrayList<Country>();
-        ArrayList<Country> TestList1 = new ArrayList<Country>();
-        ArrayList<Country> TestList2 = new ArrayList<Country>();
-        ArrayList<Country> TestList3 = new ArrayList<Country>();
-        ArrayList<Country> TestList4 = new ArrayList<Country>();
-        ArrayList<Country> TestList5 = new ArrayList<Country>();
-        ArrayList<Country> TestList6 = new ArrayList<Country>();
-        ArrayList<Country> TestList7 = new ArrayList<Country>();
-        ArrayList<Country> TestList8 = new ArrayList<Country>();
-        ArrayList<Country> TestList9 = new ArrayList<Country>();
-        ArrayList<Country> TestList10 = new ArrayList<Country>();
-        ArrayList<Country> TestList11 = new ArrayList<Country>();
-        ArrayList<Country> TestList12 = new ArrayList<Country>();
 
         // separates each file line into strings
         while(inputFile.hasNext()) {
@@ -102,23 +79,6 @@ public class Lab4 {
                     Double.parseDouble(split[32]), Double.parseDouble(split[33]));
             // adds object Country to ArrayList
             CountryList.add(info);
-            // creating new ArrayLists so that when testing my sort method on various sizes of N, the program
-            // will not be running through an already sorted list.
-            TestList.add(info);
-            TestList1.add(info);
-            TestList2.add(info);
-            TestList3.add(info);
-            TestList4.add(info);
-            TestList5.add(info);
-            TestList6.add(info);
-            TestList7.add(info);
-            TestList8.add(info);
-            TestList9.add(info);
-            TestList10.add(info);
-            TestList11.add(info);
-            TestList12.add(info);
-
-
         }
         // close FileInputStream
         in.close();
@@ -133,72 +93,6 @@ public class Lab4 {
         sort(CountryList, CountryList.size(), new CountryByExpectancy());
         double end = System.nanoTime();
         outFS.println("Runtime in nanoseconds for size " + CountryList.size() + ": " + (end - start));
-
-        // using sort to find  runtime for a variety of n
-        start = System.nanoTime();
-        sort(TestList, 185, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 185: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList1, 170, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 170: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList2, 155, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 155: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList3, 140, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 140: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList4, 125, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 125: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList5, 110, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 110: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList6, 95, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 95: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList7, 80, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 80: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList8, 65, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 65: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList9, 50, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 50: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList10, 35, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 35: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList11, 20, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 20: " + (end - start));
-
-        start = System.nanoTime();
-        sort(TestList12, 5, new CountryByExpectancy());
-        end = System.nanoTime();
-        outFS.println("Runtime in nanoseconds for size 5: " + (end - start));
 
         // prints countries using toString method in order from lowest life expectancy (starting at NA)
             // to highest life expectancy
